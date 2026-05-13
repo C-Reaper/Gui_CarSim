a371f95d34db092abd7899fb8385c7ae28ac72cb
codeleaded <gnu.c.reaper@gmail.com>
2026-05-13 18:11:37 +0200

    updated

--- src/Main.c
+++ src/Main.c
@@ -8,24 +8,19 @@ void Setup(AlxWindow* w){
     ResizeAlxFont(10,10);
 
     if(Files_isFile("./data/World0.tdworld")){
-        world = TDWorld_Load("./data/World0.tdworld",Sprite_Load("./assets/City_Roads.png"));
+        world = TDWorld_Load("./data/World0.tdworld",Sprite_Load("/home/codeleaded/Graphics/CarGame/City_Roads.png"));
     }else{
-        world = TDWorld_New(100,100,Sprite_Load("./assets/City_Roads.png"));
+        world = TDWorld_New(100,100,Sprite_Load("/home/codeleaded/Graphics/CarGame/City_Roads.png"));
     }
     // (TDEngine*)TDEngine_Gasoline_New(6,100.0f,60.0f)
     // (TDEngine*)TDEngine_Diesel_New(6,100.0f,60.0f)
     // (TDEngine*)TDEngine_Electric_New(100.0f,10.0f)
-    TDWorld_LoadCar(&world,50.0f,50.0f,"./assets/Car_Green_Fast.png",AlxFont_MAKE_HIGH(16,32),(TDEngine*)TDEngine_Gasoline_New(6,100.0f,60.0f));
-
-    world.tv.Scale = (Vec2){ 0.1f,0.1f };
+    TDWorld_LoadCar(&world,0.0f,0.0f,"./assets/Car_Green_Fast.png",AlxFont_MAKE_HIGH(16,32),(TDEngine*)TDEngine_Gasoline_New(6,100.0f,60.0f));
 }
 void Update(AlxWindow* w){
-    TransformedView_HandlePanZoom(&world.tv,w->Strokes,(Vec2){ (float)w->MouseX,(float)w->MouseY });
-    TransformedView_Focus(&world.tv,&world.car.pos,(Vec2){ 0.0f,0.0f });
-    //world.car.car.sprite.w,world.car.car.sprite.h
-
     if(Stroke(ALX_MOUSE_L).PRESSED){
-        const Vec2 m_world = TransformedView_ScreenWorldPos(&world.tv,(Vec2){ (float)w->MouseX,(float)w->MouseY });
+        const Vec2 m_world = { world.car.pos.x,-world.car.pos.y };
+        
         if(m_world.x >= 0 && m_world.x < (int)world.width && m_world.y >= 0 && m_world.y < (int)world.height){
             const unsigned int tx = (unsigned int)m_world.x;
             const unsigned int ty = (unsigned int)m_world.y;
@@ -35,7 +30,8 @@ void Update(AlxWindow* w){
                 world.world[index]++;
         }
     }else if(Stroke(ALX_MOUSE_R).PRESSED){
-        const Vec2 m_world = TransformedView_ScreenWorldPos(&world.tv,(Vec2){ (float)w->MouseX,(float)w->MouseY });
+        const Vec2 m_world = { world.car.pos.x,-world.car.pos.y };
+        
         if(m_world.x >= 0 && m_world.x < (int)world.width && m_world.y >= 0 && m_world.y < (int)world.height){
             const unsigned int tx = (unsigned int)m_world.x;
             const unsigned int ty = (unsigned int)m_world.y;
@@ -45,18 +41,20 @@ void Update(AlxWindow* w){
                 world.world[index]--;
         }
     }else if(Stroke(ALX_MOUSE_M).PRESSED){
-        const Vec2 m_world = TransformedView_ScreenWorldPos(&world.tv,(Vec2){ (float)w->MouseX,(float)w->MouseY });
+        const Vec2 m_world = { world.car.pos.x,-world.car.pos.y };
+        
         if(m_world.x >= 0 && m_world.x < (int)world.width && m_world.y >= 0 && m_world.y < (int)world.height){
             const unsigned int tx = (unsigned int)m_world.x;
             const unsigned int ty = (unsigned int)m_world.y;
             const unsigned int index = ty * world.width + tx;
+            
             world.world[index] = 0U;
         }
     }
 
     if(Stroke(ALX_KEY_W).DOWN)          TDCar_Interact(&world.car,TDENGINE_GASOLINE_INTERACT_GASDOWN,(float[]){ 1.0f });
     else                                TDCar_Interact(&world.car,TDENGINE_GASOLINE_INTERACT_GASUP,NULL);
-
+    
     if(Stroke(ALX_KEY_S).DOWN)          TDCar_Break(&world.car,0.98f);
     if(Stroke(ALX_KEY_A).DOWN)          TDCar_Turn(&world.car, F32_PI * w->ElapsedTime);
     if(Stroke(ALX_KEY_D).DOWN)          TDCar_Turn(&world.car,-F32_PI * w->ElapsedTime);
@@ -66,11 +64,12 @@ void Update(AlxWindow* w){
     if(Stroke(ALX_KEY_R).PRESSED)       TDCar_Interact(&world.car,TDENGINE_GASOLINE_INTERACT_GEARUP,NULL);
     if(Stroke(ALX_KEY_F).PRESSED)       TDCar_Interact(&world.car,TDENGINE_GASOLINE_INTERACT_GEARDOWN,NULL);
     
+    if(Stroke(ALX_KEY_UP).DOWN)      world.zoom *= 1.01f;
+    if(Stroke(ALX_KEY_DOWN).DOWN)    world.zoom *= 0.99f;
+    
     TDCar_Update(&world.car,w->ElapsedTime);
     
     Clear(BLUE);
-
-    //TDCar_Render(WINDOW_STD_ARGS,&car);
 
     TDWorld_Render(WINDOW_STD_ARGS,&world);
 
